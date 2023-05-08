6e7638c7529f22d61093cabdfe268e86b65baf27
PinD20 <enrique.pinula@gmail.com>
2023-05-08 10:24:24 -0600

    Sesión 2

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hola, mi primer javascript 222")
+
+
+function mifuncion() {
+    let titulo = document.getElementById("titulo1");
+    titulo.innerHTML = "Cambio 2";
+    titulo.style.fontSize = "50px";
+}
+
+function funcion2 () {
+    alert("Aceptado con éxito");
+    
+}
